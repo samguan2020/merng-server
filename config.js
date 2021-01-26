e4607d92d736a8182c9da79148fa6cd66468c8d3
Xin Guan <xin.guan@gmail.com>
2021-01-25 21:16:12 -0800

    fix config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB: 
+    'mongodb+srv://dbUser:6XZGmrhNPmc4EarU@cluster0.uu1vg.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY:
+    'Xin Guan Really Rocks'
+};
