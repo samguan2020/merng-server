070bac2653a0b659441f3f4fae911bd1a67d55c8
samguan2020 <63484729+samguan2020@users.noreply.github.com>
2021-09-18 17:42:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   MONGODB: 
     'mongodb+srv://samguan:q0yvwhejkSbfhBuI@cluster0.uu1vg.mongodb.net/merng?retryWrites=true&w=majority',
   SECRET_KEY:
-    'Xin Guan Really Rocks'
+    '关心真棒 英文，第一个字母大写，空格，名姓'
 };
